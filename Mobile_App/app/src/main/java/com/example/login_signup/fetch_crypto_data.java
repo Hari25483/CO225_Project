c182e41fd8584cdbf08dc0e03a30251058b94943
Hari25483 <haricodmw@gmail.com>
2022-03-06 23:10:15 +0530

    Create fetch_data_page

---
+++ Mobile_App/app/src/main/java/com/example/login_signup/fetch_crypto_data.java
@@ -0,0 +1,14 @@
+package com.example.login_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fetch_crypto_data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fetch_crypto_data);
+    }
+}
