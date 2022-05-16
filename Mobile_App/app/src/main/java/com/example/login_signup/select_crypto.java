405f6a61cc8fd7b9bf4fb2d7d36f2f66d8907c51
Hari25483 <haricodmw@gmail.com>
2022-05-16 19:15:10 +0530

    Select items

--- Mobile_App/app/src/main/java/com/example/login_signup/select_crypto.java
+++ Mobile_App/app/src/main/java/com/example/login_signup/select_crypto.java
@@ -3,10 +3,13 @@ package com.example.login_signup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class select_crypto extends AppCompatActivity {
+public class select_crypto extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +20,18 @@ public class select_crypto extends AppCompatActivity {
                 android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Crypto));
         myAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         myspinner.setAdapter(myAdaptor);
+        myspinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String crypto=adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(),crypto,Toast.LENGTH_SHORT).show();
+        System.out.println(crypto);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
