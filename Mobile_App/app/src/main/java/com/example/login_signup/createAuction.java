e762115ec1a270a1e86d9e3b4e0dc1231acdda6a
Yogeswaran Thulasiyan <73667144+SyThulasi@users.noreply.github.com>
2022-05-17 16:16:42 +0530

    add the create auction page

---
+++ Mobile_App/app/src/main/java/com/example/login_signup/createAuction.java
@@ -0,0 +1,15 @@
+package com.example.login_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createAuction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_auction);
+        m_
+    }
+}
