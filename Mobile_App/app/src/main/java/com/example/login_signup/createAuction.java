ca92e06cd6acec0c51e624fd6223faf2da0e6a3a
Yogeswaran Thulasiyan <73667144+SyThulasi@users.noreply.github.com>
2022-05-17 19:22:22 +0530

    edit and print the create auction

--- Mobile_App/app/src/main/java/com/example/login_signup/createAuction.java
+++ Mobile_App/app/src/main/java/com/example/login_signup/createAuction.java
@@ -3,13 +3,15 @@ package com.example.login_signup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class createAuction extends AppCompatActivity {
 
     private EditText auctionName, startDate,startTime, endDate, endTime ;
-
+    private Button create;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,16 @@ public class createAuction extends AppCompatActivity {
         startTime = (EditText)findViewById(R.id.editTextTime);
         endDate = (EditText)findViewById(R.id.editTextDate2);
         endTime = (EditText)findViewById(R.id.editTextTime2);
+        create = (Button) findViewById(R.id.button);
 
-    }
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println(auctionName);
+                System.out.println(startDate);
+                System.out.println(startTime);
+                System.out.println(endDate);
+                System.out.println(endTime);
+            }
+        });}
 }
