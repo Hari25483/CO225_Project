10056d17c06140e509a7dc84cffb921ea399a3b5
Hari25483 <haricodmw@gmail.com>
2022-05-18 19:42:21 +0530

    Update select_crypto.java

--- Mobile_App/app/src/main/java/com/example/login_signup/select_crypto.java
+++ Mobile_App/app/src/main/java/com/example/login_signup/select_crypto.java
@@ -81,61 +81,7 @@ public class select_crypto extends AppCompatActivity implements AdapterView.OnIt
          cryptocoin_name=adapterView.getItemAtPosition(i).toString();
 //        Toast.makeText(adapterView.getContext(),price,Toast.LENGTH_SHORT).show();
         System.out.println(cryptocoin_name);
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    URL url = new URL("https://min-api.cryptocompare.com/data/pricemulti?fsyms="+cryptocoin_name+"&tsyms=USD&api_keyR=708ad7c2a133986474a2585a9a36faffa9248de78fd6a3bd6284f16d460f7055");
-//
-//                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//                    conn.setRequestMethod("GET");
-//                    conn.connect();
-//
-//                    //Check if connect is made
-//                    int responseCode = conn.getResponseCode();
-//
-//                    // 200 OK
-//                    if (responseCode != 200) {
-//                        throw new RuntimeException("HttpResponseCode: " + responseCode);
-//                    } else {
-//
-//                        StringBuilder informationString = new StringBuilder();
-//                        Scanner scanner = new Scanner(url.openStream());
-//
-//                        while (scanner.hasNext()) {
-//                            informationString.append(scanner.nextLine());
-//                        }
-//                        //Close the scanner
-//                        scanner.close();
-//
-//                        System.out.println("data :"+informationString);
-//
-//
-//                        //JSON simple library Setup with Maven is used to convert strings to JSON
-////                JSONParser parse = new JSONParser();
-////                JSONArray dataObject = (JSONArray) parse.parse(String.valueOf(informationString));
-//                        JSONParser parser = new JSONParser();
-//                        Object obj  = parser.parse(String.valueOf(informationString));
-//                        JSONArray array = new JSONArray();
-//                        array.add(obj);
-//
-//                        //Get the first JSON object in the JSON array
-//                        System.out.println(array.get(0));
-//
-//                        JSONObject countryData = (JSONObject) array.get(0);
-//
-//                        HashMap<String, Double> capitalCities = new HashMap<String, Double>();
-//
-//                        capitalCities= (HashMap<String, Double>) countryData.get(cryptocoin_name);
-//                        System.out.println(capitalCities.get("USD"));
-//                        price= capitalCities.get("USD").toString();
-//                        System.out.println(price);
-//                    }
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }).start();
+
 }
 
     @Override
